643f1a024529e0f690e886cd7f85e8b0f75d4248
ksn199ms <mubashirpannur123@gmail.com>
2025-03-01 17:24:38 +0530

    assets are defined like JSON

---
+++ front-end/src/assets/asset.js
@@ -0,0 +1,154 @@
+import auroraBlock2First from './sofa-legs/auroraBlock2/auroraBlock2-1.jpg';
+import auroraBlock2Second from './sofa-legs/auroraBlock2/auroraBlock2-2.jpg';
+import auroraBlock2Third from './sofa-legs/auroraBlock2/auroraBlock2-3.jpg';
+import auroraBlock2Fourth from './sofa-legs/auroraBlock2/auroraBlock2-4.jpg';
+
+import auroraBlock3First from './sofa-legs/auroraBlock3/auroraBlock3-1.jpg';
+import auroraBlock3Second from './sofa-legs/auroraBlock3/auroraBlock3-2.jpg';   
+import auroraBlock3Third from './sofa-legs/auroraBlock3/auroraBlock3-3.jpg';
+import auroraBlock3Fourth from './sofa-legs/auroraBlock3/auroraBlock3-4.jpg';
+
+import auroraBlock4First from './sofa-legs/auroraBlock4/auroraBlock4-1.jpg';
+import auroraBlock4Second from './sofa-legs/auroraBlock4/auroraBlock4-2.jpg';
+import auroraBlock4Third from './sofa-legs/auroraBlock4/auroraBlock4-3.jpg';
+import auroraBlock4Fourth from './sofa-legs/auroraBlock4/auroraBlock4-4.jpg';
+
+import kulfiIceGold4First from './sofa-legs/kulfiIceGold4/kulfiIceGold4-1.jpg';
+import kulfiIceGold4Second from './sofa-legs/kulfiIceGold4/kulfiIceGold4-2.jpg';
+import kulfiIceGold4Third from './sofa-legs/kulfiIceGold4/kulfiIceGold4-3.jpg';
+
+import kulfiIceGold5First from './sofa-legs/kulfiIceGold5/kulfiIceGold5-1.jpg';
+import kulfiIceGold5Second from './sofa-legs/kulfiIceGold5/kulfiIceGold5-2.jpg';
+import kulfiIceGold5Third from './sofa-legs/kulfiIceGold5/kulfiIceGold5-3.jpg';
+
+import opulentDecoFirst from './sofa-legs/opulentDeco/opulentDeco-1.jpg';
+import opulentDecoSecond from './sofa-legs/opulentDeco/opulentDeco-2.jpg';
+import opulentDecoThird from './sofa-legs/opulentDeco/opulentDeco-3.jpg';
+import opulentDecoFourth from './sofa-legs/opulentDeco/opulentDeco-4.jpg';
+
+import regalPrism3First from './sofa-legs/regalPrism3/regalPrism3-1.jpg';
+import regalPrism3Second from './sofa-legs/regalPrism3/regalPrism3-2.jpg';
+import regalPrism3Third from './sofa-legs/regalPrism3/regalPrism3-3.jpg';
+import regalPrism3Fourth from './sofa-legs/regalPrism3/regalPrism3-4.jpg';
+
+import regalPrism4First from './sofa-legs/regalPrism4/regalPrism4-1.jpg';
+import regalPrism4Second from './sofa-legs/regalPrism4/regalPrism4-2.jpg';
+import regalPrism4Third from './sofa-legs/regalPrism4/regalPrism4-3.jpg';   
+import regalPrism4Fourth from './sofa-legs/regalPrism4/regalPrism4-4.jpg';
+
+import silverApexCorner2First from './sofa-legs/silverApexCorner2/silverApexCorner2-1.jpg';
+import silverApexCorner2Second from './sofa-legs/silverApexCorner2/silverApexCorner2-2.jpg';
+import silverApexCorner2Third from './sofa-legs/silverApexCorner2/silverApexCorner2-3.jpg';
+import silverApexCorner2Fourth from './sofa-legs/silverApexCorner2/silverApexCorner2-4.jpg';
+
+import silverApexCorner3First from './sofa-legs/silverApexCorner3/silverApexCorner3-1.jpg';
+import silverApexCorner3Second from './sofa-legs/silverApexCorner3/silverApexCorner3-2.jpg';
+import silverApexCorner3Third from './sofa-legs/silverApexCorner3/silverApexCorner3-3.jpg';
+import silverApexCorner3Fourth from './sofa-legs/silverApexCorner3/silverApexCorner3-4.jpg';
+
+import titaniumCoreFirst from './sofa-legs/titaniumCore/titaniumCore-1.jpg';
+import titaniumCoreSecond from './sofa-legs/titaniumCore/titaniumCore-2.jpg';
+import titaniumCoreThird from './sofa-legs/titaniumCore/titaniumCore-3.jpg';
+
+
+export const sofaLegsList = [
+    {
+        _id: "1",
+        name: "Aurora Block 2",
+        price: 999,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        sizes: ["2x2", "3x3", "4x4"],
+        images: [auroraBlock2First, auroraBlock2Second, auroraBlock2Third, auroraBlock2Fourth],
+        category: "sofa-legs",
+    },
+    {
+        _id: "2",
+        name: "Aurora Block 3",
+        price: 1299,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        sizes: ["2x2", "3x3", "4x4"],
+        images: [auroraBlock3First, auroraBlock3Second, auroraBlock3Third, auroraBlock3Fourth],
+        category: "sofa-legs",
+    },
+    {
+        _id: "3",
+        name: "Aurora Block 4",
+        price: 1499,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        sizes: ["2x2", "3x3", "4x4"],
+        images: [auroraBlock4First, auroraBlock4Second, auroraBlock4Third, auroraBlock4Fourth],
+        category: "sofa-legs",
+    },
+    {
+        _id: "4",
+        name: "Kulfi Ice Gold 4",
+        price: 1999,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        sizes: ["2x2", "3x3", "4x4"],
+        images: [kulfiIceGold4First, kulfiIceGold4Second, kulfiIceGold4Third],
+        category: "sofa-legs",
+    },
+    {
+        _id: "5",
+        name: "Kulfi Ice Gold 5",
+        price: 2499,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        sizes: ["2x2", "3x3", "4x4"],
+        images: [kulfiIceGold5First, kulfiIceGold5Second, kulfiIceGold5Third],
+        category: "sofa-legs",
+    },
+    {
+        _id: "6",
+        name: "Opulent Deco",
+        price: 2999,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        sizes: ["2x2", "3x3", "4x4"],
+        images: [opulentDecoFirst, opulentDecoSecond, opulentDecoThird, opulentDecoFourth],
+        category: "sofa-legs",
+    },
+    {
+        _id: "7",
+        name: "Regal Prism 3",
+        price: 3499,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        sizes: ["2x2", "3x3", "4x4"],
+        images: [regalPrism3First, regalPrism3Second, regalPrism3Third, regalPrism3Fourth],
+        category: "sofa-legs",
+    },
+    {
+        _id: "8",
+        name: "Regal Prism 4",
+        price: 3999,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        sizes: ["2x2", "3x3", "4x4"],
+        images: [regalPrism4First, regalPrism4Second, regalPrism4Third, regalPrism4Fourth],
+        category: "sofa-legs",
+    },
+    {
+        _id: "9",
+        name: "Silver Apex Corner 2",
+        price: 4499,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        sizes: ["2x2", "3x3", "4x4"],
+        images: [silverApexCorner2First, silverApexCorner2Second, silverApexCorner2Third, silverApexCorner2Fourth],
+        category: "sofa-legs",
+    },
+    {
+        _id: "10",
+        name: "Silver Apex Corner 3",
+        price: 4999,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        sizes: ["2x2", "3x3", "4x4"],
+        images: [silverApexCorner3First, silverApexCorner3Second, silverApexCorner3Third, silverApexCorner3Fourth],
+        category: "sofa-legs",
+    },
+    {
+        _id: "11",
+        name: "Titanium Core",
+        price: 5499,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        sizes: ["2x2", "3x3", "4x4"],
+        images: [titaniumCoreFirst, titaniumCoreSecond, titaniumCoreThird],
+        category: "sofa-legs",
+    },
+]
